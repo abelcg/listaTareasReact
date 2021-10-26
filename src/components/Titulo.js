a0fe9b45becf9ea2fec16259308131a148292c62
abelcg <abelcg86@gmail.com>
2021-10-26 11:29:17 -0300

    se creo la esctructura basica del proyecto de react

---
+++ src/components/Titulo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+// se autocompleta con rcc
+class Titulo extends Component {
+    render() {
+        return (
+            // aqui agrego todo el maquetado y podría tener algo de lógica
+           <h1>Lista de tareas</h1>
+        );
+    }
+}
+
+export default Titulo;
