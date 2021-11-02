cdc63965c5a51738c2505984b350ad8afb66f12a
abelcg <abelcg86@gmail.com>
2021-11-02 10:08:25 -0300

    se dio estilo al body y a los componetes. Se agrego dos props a subtitulo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import "./App.css";
 import Subtitulo from "./components/Subtitulo";
 import Titulo from "./components/Titulo";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
    <>
     <Titulo></Titulo>
-    <Subtitulo/>
+    <Subtitulo comision = '16i' estado = {true} />
    </>
   );
 }
--- src/components/Titulo.js
+++ src/components/Titulo.js
@@ -6,7 +6,7 @@ class Titulo extends Component {
     render() {
         return (
             // aqui agrego todo el maquetado y podría tener algo de lógica
-           <h1>Lista de tareas</h1>
+           <h1 className = 'display-3 text-light text-center my-5'>Lista de tareas</h1>
         );
     }
 }
